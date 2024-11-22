0dc659f16f14286c27090dad0a5110c1bb462d34
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2024-11-22 09:45:22 -0400

    code updates

--- code/04_RVSpeciesAnalysis.R
+++ code/04_RVSpeciesAnalysis.R
@@ -1,4 +1,4 @@
-##Anlaysis of RV data
+##Analysis of RV data
 
 # Load libraries ----
 library(tidyverse)
@@ -65,7 +65,26 @@ cod_plots$point_plot #the plot you can make better
 cod_plots$sp_df #the data you could use to make the plot better!
 
 rv_fish <- rv_df%>%
-           filter(SCI_NAME %in% focal_sp$SCI_NAME)
+           #filter(SCI_NAME %in% focal_sp$SCI_NAME)
+           filter(SCI_NAME == "MELANOGRAMMUS AEGLEFINUS",
+                  !is.na(dist_50),
+                  classification %in% c("WSS/Outer BoF","WSS: Banks/Inner BoF"))
+
+boot_fish <- boot_dat%>%
+             filter(species=="MELANOGRAMMUS AEGLEFINUS",
+                    distance_category == c(0,50))
+
+boot_fish%>%
+  group_by(classification,period,distance_category)%>%
+  summarise(meanw=mean(TOTWGT,na.rm=T),
+            meanc=mean(TOTNO,na.rm=T))
+
+rv_fish%>%
+  data.frame()%>%
+  group_by(classification,period,dist_50)%>%
+  summarise(meanw=mean(TOTWGT,na.rm=T),
+            meanc=mean(TOTNO,na.rm=T))%>%
+  ungroup()%>%data.frame()
 
 ## run bootstrap analysis on the rv data
 
@@ -101,15 +120,96 @@ rv_fish <- rv_df%>%
 load("output/bootstrapped_differences.RData")
 
 boot_df <- boot_dat%>%
+           mutate(wgt_ratio = wgt_target/wgt_reference,
+                  count_ratio = count_target/count_reference)%>%
            group_by(species,distance_category,classification,period)%>%
-           summarise(mean_diff_wgt = mean(prec_wgt_diff,na.rm=T),
-                     lower_diff_wgt = quantile(prec_wgt_diff,0.1,na.rm=T),
-                     upper_diff_wgt = quantile(prec_wgt_diff,0.9,na.rm=T),
-                     mean_diff_cnt = mean(prec_count_diff,na.rm=T),
+           summarise(mean_wgt_ratio = mean(wgt_ratio,na.rm=T),
+                     sd_wgt_ratio = sd(wgt_ratio,na.rm=T),
+                     
+                     mean_count_ratio = mean(count_ratio,na.rm=T),
+                     sd_count_ratio = sd(count_ratio,na.rm=T))%>%
+           ungroup()%>%
+           data.frame()%>%
+           filter(!is.na(mean_wgt_ratio),!is.na(mean_count_ratio))
+
+test_df <- boot_df%>%filter(distance_category %in% c(0,50,100),
+                            #species %in% c("MELANOGRAMMUS AEGLEFINUS","GADUS MORHUA")
+                            )%>%
+  mutate(distance_category = factor(distance_category))
+
+
+ggplot(test_df, aes(x = mean_wgt_ratio, y = distance_category, group = interaction(distance_category, period))) +
+  geom_errorbar(aes(xmin = mean_wgt_ratio - sd_wgt_ratio, xmax = mean_wgt_ratio + sd_wgt_ratio),
+                width = 0, position = position_dodge(width = 0.3)) +  # Dodging the error bars horizontally
+  geom_point(fill = "white", size = 3, shape = 21, position = position_dodge(width = 0.3)) +  # Dodging the first points horizontally
+  geom_point(aes(fill = distance_category, alpha = period), size = 3, shape = 21, position = position_dodge(width = 0.3)) +  # Dodging the second points horizontally
+  facet_grid(species ~ classification) +
+  scale_alpha_manual(values = c(0.2, 0.8)) +
+  scale_x_continuous(limits = c(NA, NA)) +  # Fill in the limits here as needed
+  labs(x = "Mean Difference Weight (scaled)", y = "Buffer (km)",
+       fill = "Period", shape = "") +
+  geom_vline(xintercept = 1, lty = 2, lwd = 0.4) +
+  theme_bw() +
+  theme(legend.position = "top",
+        strip.background = element_rect(fill = "white"))
+
+
+
+boot_df <- boot_dat%>%
+           group_by(species,distance_category,classification,period)%>%
+           summarise(mean_diff_wgt = mean(prec_wgt_diff,na.rm=T)/100,
+                     sd_diff_wgt = sd(prec_wgt_diff,na.rm=T)/100, 
+                     lower_diff_wgt = quantile(prec_wgt_diff,0.1,na.rm=T)/100,
+                     upper_diff_wgt = quantile(prec_wgt_diff,0.9,na.rm=T)/100,
+                    
+                     mean_diff_cnt = mean(prec_count_diff,na.rm=T)/100,
+                     sd_diff_cnt = sd(prec_count_diff,na.rm=T)/100, 
                      lower_diff_cnt = quantile(prec_count_diff,0.1,na.rm=T),
                      upper_diff_cnt = quantile(prec_count_diff,0.9,na.rm=T))%>%
            ungroup()%>%
            data.frame()
+
+
+test_df <- boot_df%>%filter(distance_category %in% c(0,50,100),classification =="WSS/Outer BoF",
+                            species %in% c("MELANOGRAMMUS AEGLEFINUS","GADUS MORHUA"))%>%
+           mutate(distance_category = paste(distance_category,"(km)",sep=" "))
+
+dodge_width <- 0.3
+
+ggplot(test_df, aes(x = mean_diff_wgt, y = distance_category)) +
+  geom_line(aes(group = distance_category,col=distance_category), linewidth = 0.5,show.legend = FALSE) +
+  geom_point(fill="white",size=3,shape=21)+
+  geom_point(aes(fill = distance_category, alpha=period),size=3,shape=21)+
+  facet_grid(species~.)+
+  scale_alpha_manual(values = c(0.2,0.8))+
+  #scale_fill_manual(values = c("white", "black")) + # Fill: white for one period, black for the other
+  labs(x = "Mean Difference Weight (scaled)", y = "Buffer (km)",
+       fill = "Buffer (km)", shape = "") +
+  geom_vline(xintercept=1,lty=2,lwd=0.4)+
+  theme_bw() +
+  theme(legend.position = "top",
+        strip.background = element_rect(fill="white"))
+
+ggplot(test_df, aes(x = mean_diff_wgt, y = distance_category, group = interaction(distance_category, period))) +
+  geom_errorbar(aes(xmin = mean_diff_wgt - sd_diff_wgt, xmax = mean_diff_wgt + sd_diff_wgt),
+                width = 0, position = position_dodge(width = 0.3)) +  # Dodging the error bars horizontally
+  geom_point(fill = "white", size = 3, shape = 21, position = position_dodge(width = 0.3)) +  # Dodging the first points horizontally
+  geom_point(aes(fill = distance_category, alpha = period), size = 3, shape = 21, position = position_dodge(width = 0.3)) +  # Dodging the second points horizontally
+  facet_grid(species ~ .) +
+  scale_alpha_manual(values = c(0.2, 0.8)) +
+  scale_x_continuous(limits = c(NA, NA)) +  # Fill in the limits here as needed
+  labs(x = "Mean Difference Weight (scaled)", y = "Buffer (km)",
+       fill = "Period", shape = "") +
+  geom_vline(xintercept = 1, lty = 2, lwd = 0.4) +
+  theme_bw() +
+  theme(legend.position = "top",
+        strip.background = element_rect(fill = "white"))
+  
+
+
+
+
+
 
  ggplot(boot_df%>%filter(distance_category == 0), aes(x = mean_diff_wgt / 100, y = species, shape = period, fill = value)) +
   geom_point(position = position_dodge(width = 0.5), size = 3) +
