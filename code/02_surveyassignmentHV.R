44f39ab642a50740519e83e4648cfe75037abc91
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2025-04-01 15:18:44 -0300

    updated count plot

--- code/02_surveyassignmentHV.R
+++ code/02_surveyassignmentHV.R
@@ -11,6 +11,7 @@ library(rnaturalearth)
 library(MarConsNetData)
 library(ggspatial)
 library(viridis)
+library(patchwork)
 
 # Projections ----
 latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
@@ -38,6 +39,11 @@ bioclass <- read_sf("data/shapefiles/bioclassification_clusters.shp")%>%
     bioregion == 'MAR' & cl == 2 ~ 'Laurentian Channel/Shelf Break',
     bioregion == 'MAR' & cl == 1 ~ 'Slope'))%>%
   st_transform(CanProj)
+
+#250m contour
+contour_250 <- read_sf("data/shapefiles/contour_250.shp")%>%
+               st_transform(CanProj)%>%
+               st_intersection(bioregion)
 
 #load basemap
 basemap <- rbind(ne_states(country = "Canada",returnclass = "sf")%>%
@@ -76,7 +82,8 @@ load("data/rvdata.RData")
 
 #pull out Western Emerald Bank Polygon
 webca <- read_sf("data/shapefiles/webca.shp")%>% #note that this is different than pulling from 'network' as this shape file has two parts, which are needed to do the buffer properly
-  st_transform(CanProj)
+  st_transform(CanProj)%>%
+  st_union()
 
 
 #create different buffered distances -----
@@ -94,6 +101,11 @@ webca_100 <- webca%>%
   st_union()%>%
   st_as_sf()%>%
   st_transform(CanProj)
+
+#now do the cut outs * this was missing before
+webca_100 <- webca_100%>%st_difference(webca_50)
+
+webca_50 <- webca_50%>%st_difference(webca)
 
 
 buffer_poly <- rbind(webca%>%
@@ -142,6 +154,7 @@ ggplot()+
 
 #show how the plot works
 buffer_plot <- ggplot()+
+  geom_sf(data = contour_250,lwd=0.4,col="grey40")+
   geom_sf(data = buffer_poly , aes(fill = factor(buffer)), color = "black", alpha = 0.5) +
   geom_sf(data=bioregion,fill=NA)+
   geom_sf(data=basemap)+
@@ -156,7 +169,8 @@ buffer_plot <- ggplot()+
   coord_sf(xlim=plot_lims[c(1,3)],ylim=plot_lims[c(2,4)],expand=0)+
   theme_bw()+
   annotation_scale()+
-  labs(fill="Buffer (km)")
+  labs(fill="Buffer (km)")+
+  scale_fill_brewer(palette = "Set1")
 
 ggsave("output/buffer_plot2.0.png",buffer_plot,height=6,width=6.5,units="in",dpi=300)
 
@@ -167,15 +181,68 @@ rv_count <- rvstations%>%
   summarise(count=n())%>%
   ungroup()%>%
   data.frame()%>%
-  mutate(buffer=factor(buffer,levels=c("0","50","100","Outside")))
+  mutate(buffer=factor(buffer,levels=c("0","50","100","Outside")),
+         decade = paste0(substr(YEAR, 1, 3), "0s"))
 
-count_plot <- ggplot(data=rv_count,aes(x=YEAR,y=count,group=buffer,fill=buffer))+
+count_plot <- ggplot(data=rv_count%>%filter(buffer!="Outside"),aes(x=YEAR,y=count,group=buffer,fill=buffer))+
   geom_line(aes(col=buffer))+
   geom_point(shape=21,col="black")+
   theme_bw()+
   labs(fill="Buffer (km)",col="Buffer (km)",y="Number of stations",x="")
 
 ggsave("output/count_plot2.0.png",count_plot,height=6,width=6.5,units="in",dpi=300)
+
+count_box <- ggplot(rv_count %>% filter(buffer != "Outside"), 
+                   aes(x = decade, y = count, group = interaction(decade, buffer), fill = buffer)) +
+              geom_boxplot() +
+              geom_jitter(position = position_jitterdodge(jitter.width = 0.2), alpha = 0.5,shape=21,size=0.5) +
+              theme_bw() +
+              labs(x = "", y = "# RV sets per year", fill = "Buffer (km)") +
+              scale_fill_brewer(palette = "Set1")
+
+#now make a fancy plot
+
+plot_lims2 <- buffer_poly%>%
+              st_transform(utm)%>%
+              st_buffer(30)%>%
+              st_transform(CanProj)%>%
+              st_bbox()
+
+buffer_plot2 <- ggplot()+
+  geom_sf(data=network%>%filter(site!='Western/Emerald Banks Marine Refuge'),fill=NA)+
+  geom_sf(data = contour_250,lwd=0.4,col="grey40")+
+  geom_sf(data = buffer_poly , aes(fill = factor(buffer)), color = "black", alpha = 0.5) +
+  geom_sf(data=bioregion,fill=NA)+
+  geom_sf(data=basemap)+
+  geom_sf(data=rvstations%>%
+            filter(!is.na(distance_category),
+                   YEAR>2015), #just to thin the data out a bit
+          aes(fill=factor(distance_category)),shape=21,)+
+  geom_sf(data=rvstations%>%
+            filter(is.na(distance_category),
+                   YEAR>2015),pch=3,size=0.15,col="grey80")+
+  geom_sf(data=webca,fill=NA,col="black",lwd=1.05)+
+  coord_sf(xlim=plot_lims2[c(1,3)],ylim=plot_lims2[c(2,4)],expand=0)+
+  theme_bw()+
+  labs(fill="Buffer (km)")+
+  scale_fill_brewer(palette = "Set1")+
+  theme(legend.position = "none",
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = margin(0, 0, 0, 0))
+
+
+combo <- buffer_plot2 + count_box + plot_layout(ncol=2)
+
+
+combo <- count_box + 
+  inset_element(buffer_plot2, 
+                left = 0, 
+                bottom = 0.6, 
+                right = 0.4, 
+                top = 0.95)
+
+ggsave("output/combo_count.png",combo,height=6,width=7,units="in",dpi=300)
 
 #save a datafile so you can use it in your analyses. 
 save(rv_df,file="data/rv_data_processed2.0.RData")
